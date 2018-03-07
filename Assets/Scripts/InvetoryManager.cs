87f32bd86aaf1801332df4cf7e04c75341e18eb1
JackFury360 <gabrielfigueiredov@gmail.com>
2018-03-07 17:35:33 -0300

    Final Version
    Added some jokes kkkkk

---
+++ Assets/Scripts/InvetoryManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class InvetoryManager : MonoBehaviour {
+    public List<GameObject> invObj = new List<GameObject>();
+    //public List<int> invObjAmount = new List<int>();
+    public RectTransform[] pos;
+    private bool[] posEnabled = new bool[15];
+    public int hasSpace;
+    private Text txt;
+    private bool dropAll;
+    private VideoPlayer videoPlayer;
+    public GameObject bg;
+    // Use this for initialization
+    void Start () {
+        videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
+        txt = GameObject.Find("Description").GetComponent<Text>();
+        MakeBoolsTrue();
+        hasSpace = posEnabled.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            QuitGame();
+
+        if (GetComponent<AudioSource>().isPlaying == false)
+            bg.SetActive(true);
+
+        if (dropAll)
+        {
+            foreach (GameObject go in invObj)
+            {
+                go.GetComponent<Item>().colInv = false;
+                go.GetComponent<RectTransform>().localPosition = go.GetComponent<Item>().startPos;
+                go.GetComponent<Item>().inInventory = false;
+            }
+            hasSpace = posEnabled.Length;
+            MakeBoolsTrue();
+            invObj.Clear();
+            dropAll = false;
+        }
+	}
+
+    public void AddItem(GameObject obj)
+    {
+        for (int x = 0; x < posEnabled.Length; x++)
+        {
+            if(posEnabled[x])
+            {
+                obj.GetComponent<RectTransform>().localPosition = new Vector3(pos[x].localPosition.x, pos[x].localPosition.y, pos[x].localPosition.z);
+                obj.GetComponent<Item>().index = x;
+                posEnabled[x] = false;
+                hasSpace -= 1;
+                x = posEnabled.Length;
+            }
+        }
+        invObj.Add(obj);
+    }
+
+    public void ReAlocate(GameObject obj)
+    {
+        obj.GetComponent<RectTransform>().localPosition = new Vector3(pos[obj.GetComponent<Item>().index].localPosition.x, pos[obj.GetComponent<Item>().index].localPosition.y, pos[obj.GetComponent<Item>().index].localPosition.z);
+    }
+
+    public void DropAll()
+    {
+        dropAll = true;
+        txt.text = "";
+    }
+
+    public void DropThis(GameObject obj)
+    {
+        posEnabled[obj.GetComponent<Item>().index] = true;
+        invObj.Remove(obj.gameObject);
+        hasSpace += 1;
+        txt.text = "";
+    }
+
+    private void MakeBoolsTrue()
+    {
+        for (int y = 0; y < posEnabled.Length; y++)
+        {
+            posEnabled[y] = true;
+        }
+    }
+
+    public void Use()
+    {
+        if (GetComponent<AudioSource>().isPlaying == false)
+        {
+            bg.SetActive(false);
+            GetComponent<AudioSource>().Play();
+            videoPlayer.Play();
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string description;
+    private bool active;
+    public bool inInventory;
+    private Text txt;
+
+    private bool drag;
+    private GameObject inventory;
+
+    private RectTransform activeArea;
+    private RectTransform rect;
+
+    private InvetoryManager invManager;
+
+    public Vector3 startPos;
+
+    public int index;
+    public bool colInv;
+    // Use this for initialization
+    void Start()
+    {
+        startPos = GetComponent<RectTransform>().localPosition;
+        rect = GetComponent<RectTransform>();
+        txt = GameObject.Find("Description").GetComponent<Text>();
+        inventory = GameObject.Find("Inventory");
+        activeArea = inventory.GetComponent<RectTransform>();
+        invManager = GameObject.Find("LevelManager").GetComponent<InvetoryManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && inInventory)
+        {
+            txt.text = description;
+        }
+
+        if (drag)
+            transform.position = Input.mousePosition;
+
+        if (colInv)
+        {
+            if (drag == false && invManager.hasSpace > 0)
+            {
+                inInventory = true;
+                if (invManager.invObj.Contains(this.gameObject) == false)
+                    invManager.AddItem(this.gameObject);
+
+                else
+                    invManager.ReAlocate(this.gameObject);
+            }
+
+            else if (invManager.hasSpace == 0 && drag == false && inInventory == false)
+                rect.localPosition = startPos;
+        }
+
+        else if(colInv == false)
+        {
+            if (invManager.invObj.Contains(this.gameObject))
+            {
+                invManager.DropThis(this.gameObject);
+                txt.text = "";
+            }
+            inInventory = false;
+        }
+    }
+
+    public void ShowDescription()
+    {
+        active = true;
+    }
+
+    public void HideDescription()
+    {
+        active = false;
+    }
+
+    public void DragStart()
+    {
+        drag = true;
+        transform.SetAsLastSibling();
+    }
+
+    public void DragFinish()
+    {
+        drag = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Inventory")
+            colInv = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Inventory")
+            colInv = false;
+    }
+}
